b155e09a5d0057c942c31ef245cfcbf2c11f5fef
Jardson Alves <jardson123442@gmail.com>
2024-04-15 17:48:37 -0300

    Exercício Resolvido 1 - Parte 2 Seção 13 - Vídeo 150

---
+++ src/entities/Departmant.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Departmant {
+
+    private String name;
+
+    public Departmant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
